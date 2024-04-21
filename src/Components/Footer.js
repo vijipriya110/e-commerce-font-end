403267f895a4f1fdc889651cd762d233f9d2f899
vijipriya110 <vijipriya110@gmail.com>
2024-04-21 16:13:04 +0530

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className="py-1 bg-dark">
+            <p className="text-center text-white mt-1 ">
+                cart - 2023-2024, All Rights Reserved
+            </p>
+        </footer>
+    )
+}
